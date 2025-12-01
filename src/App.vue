a5627c92c1151b7aa03af7e84dcd133a618f4962
athul <athulsuresh198@gmail.com>
2025-12-01 16:25:18 +0530

    first commit

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<script setup>
+import ProductView from './components/ProductList.vue'
+</script>
+
+<template>
+  <h1>Inventory Management</h1>
+  <ProductView />
+</template>
+
+<style scoped></style>
---
+++ src/components/ProductList.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <h1>Products list</h1>
+
+    <ul>
+      <li v-for="product in products" :key="product?.id">{{ product?.name }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import axios from 'axios'
+import { onMounted, ref } from 'vue'
+
+const products = ref([])
+
+onMounted(() => {
+  fetchProducts()
+})
+
+const fetchProducts = () => {
+  axios
+    .get('http://127.0.0.1:8000/api/products')
+    .then((response) => {
+      products.value = response.data.data
+    })
+    .catch((error) => console.log(error))
+}
+</script>
